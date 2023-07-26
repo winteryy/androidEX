906989de2306457ce233dab9cf056f2ae4cf4b0c
Lee HuiGwang <104819764+winteryy@users.noreply.github.com>
2023-07-27 02:47:11 +0900

    tiny fix

--- Jetpack_EX/app/src/main/java/com/example/jetpackex/dataBinding/DbCustomViewAdapter.kt
+++ Jetpack_EX/app/src/main/java/com/example/jetpackex/dataBinding/DbCustomViewAdapter.kt
@@ -3,7 +3,9 @@ package com.example.jetpackex.dataBinding
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.example.jetpackex.R
 import com.example.jetpackex.databinding.TextRowItemBinding
 
 class DbCustomViewAdapter(val dataSet: ArrayList<String>) : RecyclerView.Adapter<DbCustomViewAdapter.ViewHolder>() {
@@ -17,7 +19,8 @@ class DbCustomViewAdapter(val dataSet: ArrayList<String>) : RecyclerView.Adapter
         parent: ViewGroup,
         viewType: Int
     ): DbCustomViewAdapter.ViewHolder {
-        val view = TextRowItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val view = DataBindingUtil.inflate<TextRowItemBinding>(LayoutInflater.from(parent.context),
+        R.layout.text_row_item, parent, false)
 
         return ViewHolder(view)
     }
